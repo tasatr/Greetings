319276ff9a055201560f01647e566cec11336cbb
Triinu Tasa <triinu.tasa@gmail.com>
2021-10-20 15:31:08 +0300

    Adding smart contract for ebay clone

---
+++ contracts/EbayClone.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.5.16;
+contract EBayClone {
+  struct Product {
+    uint id;
+    address payable seller;
+    address payable buyer;
+    string name;
+    string description;
+    uint price;
+  }
+
+  uint productCounter;
+  mapping (uint => Product) public products;
+
+  function sellProduct(string memory _name, string memory _description, uint _price) public {
+    Product memory newProduct = Product({
+      id: productCounter,
+      seller: msg.sender,
+      buyer: address(0x0),
+      name: _name,
+      description: _description,
+      price: _price
+    });
+
+    products[productCounter] = newProduct;
+    productCounter++;
+  }
+
+  function getNumberOfProducts() public view returns (uint) {
+    return productCounter;
+  }
+
+  function buyProduct (uint _id) payable public {
+    Product storage product = products[_id];
+    require(product.buyer == address(0x0));
+    require(msg.sender != product.seller);
+    require(msg.value == product.price);
+    product.buyer = msg.sender;
+    product.seller.transfer(msg.value);
+  }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,6 @@
 const GreetingsContract = artifacts.require("Greetings");
+const EBayContract = artifacts.require("EBayClone");
 module.exports = function(deployer) {
     deployer.deploy(GreetingsContract);
+    deployer.deploy(EBayContract);
 };
